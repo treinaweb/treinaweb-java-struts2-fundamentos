3945a88fa23c72d8e4566d07673cd8d10b8699b9
Cleber Campomori <cleber.campomori@gmail.com>
2019-04-23 21:15:07 -0300

    Aula 4.1: Compreendendo e definindo actions no Struts2 - parte 1

---
+++ tw-struts2-fundamentos/src/main/java/br/com/treinaweb/struts2/actions/TesteAction.java
@@ -0,0 +1,17 @@
+package br.com.treinaweb.struts2.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TesteAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7432816271355151265L;
+	
+	@Override
+	public String execute() {
+		return "TESTE";
+	}
+
+}
